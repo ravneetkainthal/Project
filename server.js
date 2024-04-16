4b04e59d91e08724ac125871ea531affd4525cb9
Ravneet <ravneetkainthal@gmail.com>
2024-04-16 15:26:20 -0400

    changes

--- server.js
+++ server.js
@@ -269,6 +269,6 @@ app.post('/api/Movies', (req, res) => {
 app.use(authMovies);
 
 
-app.listen(3000, () => {
-  console.log(`Server is running on http://localhost:3000`);
+app.listen(8000, () => {
+  console.log(`Server is running on http://localhost:8000`);
 });
